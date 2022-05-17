14d9a4a5cce4c41af284d72a1649aa89c5b23277
rlaalrud <49364743+rlaalrud@users.noreply.github.com>
2022-05-17 17:56:48 +0900

    더 줄일 수 있는 방법이 없을까..

---
+++ 202205W3/picking-numbers/mk.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'pickingNumbers' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int pickingNumbers(List<Integer> a) {
+        // Write your code here
+        int[] countNum = new int[100];
+        
+        for(int i = 0; i < a.size(); i ++) {
+            countNum[a.get(i)] += 1;
+        }
+        
+        int result = 0;
+        for(int j = 1; j < 100; j++) {
+            int sum = countNum[j - 1] + countNum[j];
+            if(result < sum) {
+                result = sum;
+            }
+        }
+
+        return result;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.pickingNumbers(a);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
