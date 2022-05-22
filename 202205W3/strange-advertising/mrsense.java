7be2ae2ec470667a48fe947572b802882a248966
mr-sense <jongrakgoodguy@gmail.com>
2022-05-23 02:10:58 +0900

    #14 쉬운데 어렵다

---
+++ 202205W3/strange-advertising/mrsense.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'viralAdvertising' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static int viralAdvertising(int n) {
+        // Write your code here
+        int share = 5;
+        int like = 2;
+        if (n == 1) {
+            return like;
+        }
+        
+        int likeSum = like;
+        for (int i = 2; i <= n; i++) {
+            share = like * 3;
+            like = share / 2;
+            likeSum += like;
+        }
+        
+        return likeSum;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int result = Result.viralAdvertising(n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
